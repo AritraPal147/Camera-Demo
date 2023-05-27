e1ec4f43d32ef9f35de81a053d6c5af659760747
Aritra Pal <94114400+AritraPal147@users.noreply.github.com>
2023-05-27 17:10:22 +0530

    Added CameraController

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  /// Ensures that the plugin services are initialized.
+  WidgetsFlutterBinding.ensureInitialized();
+
+  /// Obtain a list of the available cameras on the device.
+  final cameras = await availableCameras();
+
+  /// Get a specific camera from the list of available cameras.
+  final firstCamera = cameras.first;
+
   runApp(
     MaterialApp(
       title: 'Flutter Application',
@@ -8,20 +18,51 @@ void main() {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: const HomePage(),
+      home: TakePictureScreen(
+        camera: firstCamera,
+      ),
     ),
   );
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class TakePictureScreen extends StatefulWidget {
+  const TakePictureScreen({Key? key, required this.camera}) : super(key: key);
+
+  final CameraDescription camera;
+
+  @override
+  State<TakePictureScreen> createState() => _TakePictureScreenState();
+}
+
+class _TakePictureScreenState extends State<TakePictureScreen> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    /// To display the current output of the Camera,
+    /// a CameraController is created
+    _controller = CameraController(
+      /// Gets a specific camera from the list of available cameras.
+      widget.camera,
+      /// Defines the resolution to be used.
+      ResolutionPreset.medium,
+    );
+
+    /// Initialize the controller. This returns a Future
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    /// Disposes the controller when the widget is disposed.
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-      ),
-    );
+    return const Placeholder();
   }
 }
